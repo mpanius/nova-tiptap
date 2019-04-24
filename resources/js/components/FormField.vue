a613f34697bf54a07deb9605473b3f10d82856b2
mpanius <mpanius@gmail.com>
2019-04-24 16:01:35 +0300

    Revert "experiment with editors"
    This reverts commit c8ac6deb

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -38,64 +38,7 @@
                         </span>
 
                     </template>
-                    <button
-                            class="menubar__button"
-                            @click="commands.createTable({rowsCount: 3, colsCount: 3, withHeaderRow: false })"
-                    >
-                        <icon name="table" />
-                    </button>
-
-                    <span v-if="isActive.table()">
-						<button
-                                class="menubar__button"
-                                @click="commands.deleteTable"
-                        >
-							<icon name="delete_table" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.addColumnBefore"
-                        >
-							<icon name="add_col_before" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.addColumnAfter"
-                        >
-							<icon name="add_col_after" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.deleteColumn"
-                        >
-							<icon name="delete_col" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.addRowBefore"
-                        >
-							<icon name="add_row_before" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.addRowAfter"
-                        >
-							<icon name="add_row_after" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.deleteRow"
-                        >
-							<icon name="delete_row" />
-						</button>
-						<button
-                                class="menubar__button"
-                                @click="commands.toggleCellMerge"
-                        >
-							<icon name="combine_cells" />
-						</button>
-                    </span>
-                </div>
+                  </div>
             </editor-menu-bar>
 
             <editor-content
@@ -143,10 +86,6 @@ import {
     Strike,
     Underline,
     History,
-    Table,
-    TableHeader,
-    TableCell,
-    TableRow
 } from 'tiptap-extensions'
 
 export default {
@@ -189,10 +128,6 @@ export default {
                     new Strike(),
                     new Underline(),
                     new History(),
-                    new Table(),
-                    new TableHeader(),
-                    new TableCell(),
-                    new TableRow(),
                 ],
             }),
         }
